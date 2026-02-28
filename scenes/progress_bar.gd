6cfb2b91922a0a142110ed8bef2dc392c276d1bf
AtharvanT <atami3@ocdsb.ca>
2026-02-28 18:04:07 -0500

    Changed oxygen time
    changed from 0.5 to 0.01

--- scenes/progress_bar.gd
+++ scenes/progress_bar.gd
@@ -4,7 +4,7 @@ var game_over = false
 
 func _process(delta):
 	value = UserInterface.oxygen
-	UserInterface.oxygen -= 0.5
+	UserInterface.oxygen -= 0.01
 	
 	if UserInterface.oxygen <= 0 and not game_over:
 		game_over = true
