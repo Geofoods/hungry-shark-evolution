9f6ae4f299de4fd822d20224b9e85d250ecfecad
AtharvanT <atami3@ocdsb.ca>
2026-02-28 18:01:47 -0500

    jiji
    digger

--- scenes/progress_bar.gd
+++ scenes/progress_bar.gd
@@ -1,6 +1,12 @@
 extends ProgressBar
 
+var game_over = false
+
 func _process(delta):
-	# Update ProgressBar value from global oxygen
 	value = UserInterface.oxygen
-	UserInterface.oxygen -= 0.01
+	UserInterface.oxygen -= 0.5
+	
+	if UserInterface.oxygen <= 0 and not game_over:
+		game_over = true
+		get_node("/root/level/User Interface/Sprite2D").visible = true
+		get_tree().paused = true
