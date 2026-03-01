447f9d0e4902557b6f3da984689324b32f888343
Geofoods <curious.george71511@gmail.com>
2026-03-01 00:24:09 -0500

    update

--- scenes/nextlevel.gd
+++ scenes/nextlevel.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
-var levels = ["res://scenes/level2.tscn", "res://scenes/level3.tscn", "res://scenes/level4.tscn"]
+var levels = ["res://scenes/level2.tscn", "res://scenes/level3.tscn", "res://scenes/level4.tscn", "res://scenes/level5.tscn"]
 
 func _on_body_entered(body: Node2D) -> void:
+	print(body.name)
 	if body.name == "miner":
 		UserInterface.level += 1
 		LayerPopup.show_layer(UserInterface.level)
-		var idx = clamp(UserInterface.level - 1, 0, levels.size() - 1)
-		get_tree().change_scene_to_file(levels[idx])
+		get_tree().change_scene_to_file(levels[randi_range(0,3)])
