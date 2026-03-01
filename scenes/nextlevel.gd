6f292e21176ea23baab6e2350516232e0c67d0e6
FRCJavisT <jtsai2@ocdsb.ca>
2026-02-28 19:59:57 -0500

    added layer depths

--- scenes/nextlevel.gd
+++ scenes/nextlevel.gd
@@ -5,4 +5,6 @@ var levels = ["res://scenes/level2.tscn", "res://scenes/level3.tscn"]
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "miner":
 		UserInterface.level += 1
-		get_tree().change_scene_to_file(levels[randi_range(0,1)])
+		var depth = str(snappedf(((body.position.y + 1990) * -0.01) + -44.0 * (UserInterface.level - 1), 0.1)) + "m"
+		LayerPopup.show_layer(UserInterface.level, depth)
+		get_tree().change_scene_to_file(levels[randi_range(0, 1)])
