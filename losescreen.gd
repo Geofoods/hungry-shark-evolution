95d7d63f2f92fa3f8c59a033afed7e9f348923f1
Geofoods <curious.george71511@gmail.com>
2026-02-28 22:27:36 -0500

    lose screen

--- losescreen.gd
+++ losescreen.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 func _process(delta: float) -> void:
 	if UserInterface.oxygen < 0:
 		visible = true
+		get_tree().paused = true
 
 
 func _on_button_pressed() -> void:
@@ -10,3 +11,4 @@ func _on_button_pressed() -> void:
 	UserInterface.oxygen = 100
 	UserInterface.level = 0
 	visible = false
+	get_tree().paused = false
